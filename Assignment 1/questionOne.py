22099e9d413ea266a7d614555af6003c3829c08a
Tousif Habib <thabib@mun.ca>
2023-02-11 16:31:38 -0330

    Alternative implementation

--- Assignment 1/questionOne.py
+++ Assignment 1/questionOne.py
@@ -5,44 +5,57 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 # Load the training data
-train_sNC = pd.read_csv("train.sNC.csv")
-train_sDAT = pd.read_csv("train.sDAT.csv")
+train_sNC = pd.read_csv("train.sNC.csv", header=None, delimiter=",")
+train_sDAT = pd.read_csv("train.sDAT.csv", header=None, delimiter=",")
 
 # Load the test data
-test_sNC = pd.read_csv("test.sNC.csv")
-test_sDAT = pd.read_csv("test.sDAT.csv")
+test_sNC = pd.read_csv("test.sNC.csv", header=None, delimiter=",")
+test_sDAT = pd.read_csv("test.sDAT.csv", header=None, delimiter=",")
 
 # Load the 2D grid points
-grid_points = pd.read_csv("2D_grid_points.csv")
+grid_points = pd.read_csv("2D_grid_points.csv", header=None, delimiter=",")
+grid_points.columns = ["X", "Y"]
+
 
 # Concatenate the training data and test data
 train_data = pd.concat([train_sNC, train_sDAT], axis=0)
+train_labels = np.concatenate((np.zeros(len(train_sNC)), np.ones(len(train_sDAT))))
+
 test_data = pd.concat([test_sNC, test_sDAT], axis=0)
+test_labels = np.concatenate((np.zeros(len(test_sNC)), np.ones(len(test_sDAT))))
 
-# Assign class labels to the training data and test data
-train_data["class_label"] = np.concatenate([np.zeros(236), np.ones(236)], axis=0)
-test_data["class_label"] = np.concatenate([np.zeros(99), np.ones(99)], axis=0)
 
-# Split the data into features and labels
-train_features = train_data.iloc[:, :-1]
-train_labels = train_data.iloc[:, -1]
-test_features = test_data.iloc[:, :-1]
-test_labels = test_data.iloc[:, -1]
-
-# Train and evaluate the KNN classifier for various values of k
 k_values = [1, 3, 5, 10, 20, 30, 50, 100, 150, 200]
+train_errors = []
+test_errors = []
 for k in k_values:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(train_features, train_labels)
-    train_preds = knn.predict(train_features)
-    test_preds = knn.predict(test_features)
+    knn = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+
+    # Train the classifier
+    knn.fit(train_data, train_labels)
+
+    train_preds = knn.predict(train_data)
+    test_preds = knn.predict(test_data)
+
+    # Calculate the accuracy on the training and test data
     train_accuracy = accuracy_score(train_labels, train_preds)
     test_accuracy = accuracy_score(test_labels, test_preds)
-    
-    # Plot the classification boundary and data samples
-    x_min, x_max = grid_points["x"].min() - 0.1, grid_points["x"].max() + 0.1
-    y_min, y_max = grid_points["y"].min() - 0.1, grid_points["y"].max() + 0.1
-    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
-    grid_points_features = np.c_[xx.ravel(), yy.ravel()]
-    grid_points_preds = knn.predict(grid_points_features)
-    grid_points_preds = grid_points_preds.resh
+
+    print("K Value: ", k)
+    print("Train accuracy")
+    print(train_accuracy)
+    print("Test accuracy")
+    print(test_accuracy)
+
+    # Plot the decision boundary and the data points
+    X = grid_points.iloc[:, 0].values.reshape(-1, 1)
+    Y = grid_points.iloc[:, 1].values.reshape(-1, 1)
+    Z = knn.predict(np.c_[X, Y])
+
+    plt.scatter(X, Y, c=Z)
+    plt.scatter(train_sNC[0], train_sNC[1], c='green', marker='o', label='sNC (Train)')
+    plt.scatter(train_sDAT[0], train_sDAT[1], c='blue', marker='o', label='sDAT (Train)')
+    plt.scatter(test_sNC[0], test_sNC[1], c='red', marker='+', label='sNC (Test)')
+    plt.scatter(test_sDAT[0], test_sDAT[1], c='orange', marker='+', label='sDAT (Test)')
+    plt.show()
+
