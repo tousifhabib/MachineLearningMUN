5af7d2232b65c642410078ea16b18f75627a170a
Tousif Habib <thabib@mun.ca>
2023-02-12 13:59:59 -0330

    Make code runnable and importable as module

--- Assignment 1/questionOne.py
+++ Assignment 1/questionOne.py
@@ -3,63 +3,81 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+def classify():
+    # Load the training data
+    train_sNC = pd.read_csv("train.sNC.csv", header=None, delimiter=",")
+    train_sDAT = pd.read_csv("train.sDAT.csv", header=None, delimiter=",")
 
-# Load the training data
-train_sNC = pd.read_csv("train.sNC.csv", header=None, delimiter=",")
-train_sDAT = pd.read_csv("train.sDAT.csv", header=None, delimiter=",")
+    # Load the test data
+    test_sNC = pd.read_csv("test.sNC.csv", header=None, delimiter=",")
+    test_sDAT = pd.read_csv("test.sDAT.csv", header=None, delimiter=",")
 
-# Load the test data
-test_sNC = pd.read_csv("test.sNC.csv", header=None, delimiter=",")
-test_sDAT = pd.read_csv("test.sDAT.csv", header=None, delimiter=",")
+    # Load the 2D grid points
+    grid_points = pd.read_csv("2D_grid_points.csv", header=None, delimiter=",")
+    grid_points.columns = ["X", "Y"]
 
-# Load the 2D grid points
-grid_points = pd.read_csv("2D_grid_points.csv", header=None, delimiter=",")
-grid_points.columns = ["X", "Y"]
+    # Concatenate the training data and test data
+    train_data = pd.concat([train_sNC, train_sDAT], axis=0)
+    train_labels = np.concatenate((np.zeros(len(train_sNC)), np.ones(len(train_sDAT))))
 
+    test_data = pd.concat([test_sNC, test_sDAT], axis=0)
+    test_labels = np.concatenate((np.zeros(len(test_sNC)), np.ones(len(test_sDAT))))
 
-# Concatenate the training data and test data
-train_data = pd.concat([train_sNC, train_sDAT], axis=0)
-train_labels = np.concatenate((np.zeros(len(train_sNC)), np.ones(len(train_sDAT))))
+    k_values = [1, 3, 5, 10, 20, 30, 50, 100, 150, 200]
+    train_errors = []
+    test_errors = []
+    for k in k_values:
+        knn = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
 
-test_data = pd.concat([test_sNC, test_sDAT], axis=0)
-test_labels = np.concatenate((np.zeros(len(test_sNC)), np.ones(len(test_sDAT))))
+        # Train the classifier
+        knn.fit(train_data, train_labels)
 
+        train_preds = knn.predict(train_data)
+        test_preds = knn.predict(test_data)
 
-k_values = [1, 3, 5, 10, 20, 30, 50, 100, 150, 200]
-train_errors = []
-test_errors = []
-for k in k_values:
-    knn = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+        # Calculate the accuracy on the training and test data
+        train_accuracy = accuracy_score(train_labels, train_preds)
+        test_accuracy = accuracy_score(test_labels, test_preds)
 
-    # Train the classifier
-    knn.fit(train_data, train_labels)
+        train_errors.append(1 - train_accuracy)
+        test_errors.append(1 - test_accuracy)
 
-    train_preds = knn.predict(train_data)
-    test_preds = knn.predict(test_data)
+        # Predict the labels for the 2D grid points
+        grid_preds = knn.predict(grid_points)
 
-    # Calculate the accuracy on the training and test data
-    train_accuracy = accuracy_score(train_labels, train_preds)
-    test_accuracy = accuracy_score(test_labels, test_preds)
+        # Plot the decision boundary using a contour plot
+        plt.figure()
+        plt.title("k = " + str(k) + " (Train Error: " + str(round(train_errors[-1], 4)) +
+                  ", Test Error: " + str(round(test_errors[-1], 4)) + ")")
+        plt.scatter(grid_points['X'], grid_points['Y'], c=grid_preds)
+        # plt.scatter(train_sNC[0], train_sNC[1], c='purple', marker='o', label='sNC (Train)')
+        # plt.scatter(train_sDAT[0], train_sDAT[1], c='orange', marker='x', label='sDAT (Train)')
+        plt.scatter(test_sNC[0], test_sNC[1], c='green', marker='o', label='sNC (Test)')
+        plt.scatter(test_sDAT[0], test_sDAT[1], c='blue', marker='x', label='sDAT (Test)')
+        plt.legend()
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
 
-    train_errors.append(1 - train_accuracy)
-    test_errors.append(1 - test_accuracy)
+    return k_values, train_errors, test_errors
 
-    # generate the classification boundary visualization plot
-    plt.figure()
-    plt.title("k = " + str(k) + " (Train Error: " + str(round(train_errors[-1], 4)) +
-              ", Test Error: " + str(round(test_errors[-1], 4)) + ")")
-
-    plt.scatter(grid_points.iloc[:, 0], grid_points.iloc[:, 1], c=knn.predict(grid_points),  alpha=0.5)
-    plt.scatter(train_sNC.iloc[:, 0], train_sNC.iloc[:, 1], c='purple', marker='o', label='sNC (Train)')
-    plt.scatter(train_sDAT.iloc[:, 0], train_sDAT.iloc[:, 1], c='orange', marker='x', label='sDAT (Train)')
-    plt.scatter(test_sNC.iloc[:, 0], test_sNC.iloc[:, 1], c='green', marker='o', label='sNC (Test)')
-    plt.scatter(test_sDAT.iloc[:, 0], test_sDAT.iloc[:, 1], c='blue', marker='x', label='sDAT (Test)')
-    plt.legend()
+def Q1_results():
+    print('Generating results for Q1...')
+    k_values, train_errors, test_errors = classify()
+    plt.plot(k_values, train_errors, label='Train Error')
+    plt.plot(k_values, test_errors, label='Test Error')
+    plt.xlabel('K Value')
+    plt.ylabel('Error rate')
     plt.show()
 
+def Q2_results():
+    print('Generating results for Q2...')
 
-plt.plot(k_values, train_errors, label='Train Error')
-plt.plot(k_values, test_errors, label='Test Error')
-plt.xlabel('K Value')
-plt.ylabel('Error rate')
-plt.show()
+def Q3_results():
+    print('Generating results for Q3...')
+
+if __name__ == "__main__":
+    Q1_results()
+    Q2_results()
+    Q3_results()
+
